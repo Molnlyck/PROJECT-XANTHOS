ef859b31c73ac8e9375f0441afe6ee33a189c34b
Patrick Bergmann <Patrick.bergmann96@gmail.com>
2017-08-28 09:15:58 +0200

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Datamatiker | Admin</title>
+		<meta name="author" content="Patrick Bergmann">
+		<meta name="description" content="Admin panel for https://datamatiker.vibedrive.dk/">
+		<meta name="keywords" content="datamatiker">
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="https://skole.vibedrive.dk/admin/assets/css/style.css">
+	</head>
+	<body>
+		<div class="header">
+			<div class="container">
+				<div class="navigation">
+					<nav class="navTop">
+						<ul>
+							<li><a href="./">Hjem</a></li>
+							<li><a href="add.php">Opret emne</a></li>
+							<li><a href="upload.php">Upload</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
