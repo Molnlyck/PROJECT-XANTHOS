eab289554c697398a2780d5c52be73bdee9d9265
Patrick Bergmann <Patrick.bergmann96@gmail.com>
2017-08-30 19:54:28 +0200

    Stukturorden + ekstra knap

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,59 +1,54 @@
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Datamatiker Vejle <small style="font-size:10px; margin-left:10px;">Project by: Nicklas Vilster & Patrick Bergmann</small></a>
-    </div>
-    <form class="navbar-form navbar-right">
-	  <div class="input-group">
-	    <input type="text" class="form-control" placeholder="Søg efter opgave...">
-	    <div class="input-group-btn">
-	      <button class="btn btn-default" type="submit">
-	        <i class="glyphicon glyphicon-search"></i>
-	      </button>
-	    </div>
-	  </div>
-	</form>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="https://skole.vibedrive.dk/home.php" >Hjem</a></li>
-       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">1. Semester <span class="caret"></span></a>
-        <ul class="dropdown-menu multi-level">
-          <li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-code"></i> Programmering</a>
-            <ul class="dropdown-menu dropdown-menu-thirdlevel">
-              <? include 'semester/1semester.pro.inc.php'; ?>
-            </ul>
-          </li>
-          <li class=""><a href="#"><i class="fa fa-line-chart"></i> Systemudvikling </a></li>
-          <li><a href="#"><i class="fa fa-building-o"></i> Virksomhed    </a></li>
-          <li><a href="#"><i class="fa fa-database"></i> Database</a></li>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Datamatiker Vejle <small style="font-size:10px; margin-left:10px;">Project by: Nicklas Vilster & Patrick Bergmann</small></a>
+        </div>
+        <form class="navbar-form navbar-right">
+	        <div class="input-group">
+	            <input type="text" class="form-control" placeholder="Søg efter opgave...">
+	            <div class="input-group-btn"><button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button></div>
+	        </div>
+	    </form>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="https://skole.vibedrive.dk/home.php" >Hjem</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">1. Semester <span class="caret"></span></a>
+                <ul class="dropdown-menu multi-level">
+                    <li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-code"></i> Programmering</a>
+                        <ul class="dropdown-menu dropdown-menu-thirdlevel">
+                            <? include 'semester/1semester.pro.inc.php'; ?>
+                        </ul>
+                    </li>
+                    <li class=""><a href="#"><i class="fa fa-line-chart"></i> Systemudvikling </a></li>
+                    <li><a href="#"><i class="fa fa-building-o"></i> Virksomhed    </a></li>
+                    <li><a href="#"><i class="fa fa-database"></i> Database</a></li>
+                </ul>
+            </li>
+            <li><a href="#" >2. Semester</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" >3. Semester <span class="caret"></span></a>
+      	        <ul class="dropdown-menu multi-level">
+      		        <li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-code"></i> Programmering</a>
+      			        <ul class="dropdown-menu dropdown-menu-thirdlevel">
+      				        <? include 'semester/3semester.pro.inc.php'; ?>
+      			        </ul>
+      		        </li>
+      		        <li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-line-chart"></i> Systemudvikling</a>
+      			        <ul class="dropdown-menu dropdown-menu-thirdlevel">
+      				        <? include 'semester/3semester.sys.inc.php'; ?>
+                        </ul>
+      		        </li>
+      		        <li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-html5"></i> Webudvikling</a>
+      			        <ul class="dropdown-menu dropdown-menu-thirdlevel">
+      				        <? include 'semester/3semester.web.inc.php'; ?>
+      				        <ol class="topic">
+      					        <h3>Auto Centrum</h3>
+      					        <hr>
+      					        <li><a href="https://skole.vibedrive.dk/semester/3/web/auto.php">Autofirma</a></li>
+                            </ol>
+                        </ul>
+      		        </li>
+      	        </ul>
+            </li>
+            <li><a href="#">Dokumentation</a></li>
         </ul>
-      </li>
-      <li><a href="#" >2. Semester</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" >3. Semester <span class="caret"></span></a>
-      	<ul class="dropdown-menu multi-level">
-      		<li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-code"></i> Programmering</a>
-      			<ul class="dropdown-menu dropdown-menu-thirdlevel">
-      				<? include 'semester/3semester.pro.inc.php'; ?>	
-      			</ul>
-      		</li>
-      		<li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-line-chart"></i> Systemudvikling</a>
-      			<ul class="dropdown-menu dropdown-menu-thirdlevel">
-      				<? include 'semester/3semester.sys.inc.php'; ?>	
-      			</ul>
-      		</li>
-      		<li class="dropdown-submenu"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-html5"></i> Webudvikling</a>
-      			<ul class="dropdown-menu dropdown-menu-thirdlevel">
-      				<? include 'semester/3semester.web.inc.php'; ?>
-      				<ol class="topic">
-      					<h3>Auto Centrum</h3>
-      					<hr>
-      					<li><a href="https://skole.vibedrive.dk/semester/3/web/auto.php">Autofirma</a></li>
-      				</ol>
-      			</ul>
-      		</li>
-      	</ul>
-      </li>
-      
-    </ul>
-    
-  </div>
+    </div>
 </nav>
