54f6ebf745a9e5186d760251e65f230b33d1ce33
Patrick Bergmann <Patrick.bergmann96@gmail.com>
2017-08-30 21:17:00 +0200

    Update v. 0.0.1

--- upload.php
+++ upload.php
@@ -1,10 +1,38 @@
+<? include 'include/config.php'; ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Upload en opgave | Datamatiker 2016 - 2019</title>
+        <meta name="author" content="Patrick Bergmann">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://skole.vibedrive.dk/assets/css/style.css">
+        <link rel="stylesheet" href="https://skole.vibedrive.dk/assets/css/general.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- JS -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- FAVICON -->
+        <link rel="shortcut icon" href="http://datamatiker.vibedrive.dk/assets/img/favicon.ico">
     </head>
     <body>
-        <h1>Hello World</h1>
+        <?php include 'include/header.inc.php'; ?>
+
+        <?php include 'include/upload.inc.php'; ?>
+
+        <div class="bgimg"></div>
+        <div class="overlaybg"></div>
+
+        <?php include 'include/footer.inc.php'; ?>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </body>
 </html>
