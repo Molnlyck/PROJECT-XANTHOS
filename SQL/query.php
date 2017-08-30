aca037bf2ce9e73668fdc4c0bd3e0d99364bfcbe
Patrick Bergmann <Patrick.bergmann96@gmail.com>
2017-08-30 20:19:40 +0200

    Removed extra folders

--- SQL/query.php
+++
@@ -1,9 +0,0 @@
-<?
-	$default = "SELECT * FROM $table";
-	$navigation_query = "SELECT * FROM opgave INNER JOIN topic WHERE opgave.topic = topic.topic_id ORDER BY topic.topic_name ASC";
-	$topic = "SELECT * FROM topic WHERE semester='1' AND class='programmering'";
-	$opgave = "SELECT * FROM opgave";
-	
-
-
-?>
