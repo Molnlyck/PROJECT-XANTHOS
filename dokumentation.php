58ea81e845e7529215ca5f2a93333d4ed9693b6b
Nicklas Vilster <nick5786@edu.eal.dk>
2017-08-30 22:46:55 +0200

    EDIT!!! Arbejdet på "About us" siden

--- dokumentation.php
+++ dokumentation.php
@@ -13,6 +13,7 @@ include_once 'include/config.php';
 
 
     <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/dokumentation.css">
     <link rel="stylesheet" href="https://skole.vibedrive.dk/assets/css/style.css">
     <link rel="stylesheet" href="https://skole.vibedrive.dk/assets/css/general.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -27,22 +28,54 @@ include_once 'include/config.php';
 
 </head>
 
-    <body>
-        <?php include 'include/header.inc.php'; ?>
+<body>
+
+<?php include 'include/header.inc.php'; ?>
+
+
+<div id="introContainer" class="container-fluid">
+    <!-- Introduction Row -->
+    <h1 id="introText" class="my-4">About Us
+        <small>HEJ! her kan du se lidt om os</small>
+    </h1>
+
+    <p id="introTextparagraph">Mange kaldte det umuligt, men i dag har to helte forenet deres kræfter i et fælles forsøg på at skabe en bedre vberden, og et bedre web.</p>
+
+    <!-- Team Members Row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 id="teamText" class="my-4">Our Team</h2>
+
+
+            <!-- Patrick Bergmann -->
+        </div>
+        <div class="col-lg-6 col-sm-6 text-center mb-6">
+            <img id="picture1" class="rounded-circle img-fluid d-block mx-auto" src="https://cdn.discordapp.com/attachments/350686737007116299/352526002255298570/migpaamc.jpg" alt="">
+            <h3>Patrick Bergmann
+                <small id="task">Officiel team abe</small>
+            </h3>
+            <p><a href="https://www.facebook.com/MolnlyckCoding/?fref=ts">Facebook</a> || <a href="https://twitter.com/MolnlyckCoding">Twitter</a> || <a href="https://github.com/Molnlyck">GitHub</a> </p>
+        </div>
+
+        <!-- Nicklas Vilster -->
+        <div class="col-lg-6 col-sm-6 text-center mb-6">
+            <img id="picture2" class="rounded-circle img-fluid d-block mx-auto" src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/12278730_10153247624051381_4583848965544923573_n.jpg?oh=6a2487f5c1e6fc97f4e67323eb0a102d&oe=5A1BC914" alt="">
+            <h3>Nicklas Vilster
+                <small id="task">Duden der holder styr på team aben</small>
+            </h3>
+            <p><a href="https://www.linkedin.com/in/nicklas-vilster-388704a4/">LinkedIn</a> || <a href="https://twitter.com/Chr0lic">Twitter</a> || <a href="https://github.com/Chrolic">GitHub</a> </p>
+        </div>
+
+    </div>
+</div>
+<!-- /.container -->
 
 
 
+<?php include 'include/footer.inc.php'; ?>
 
-
-
-
-
-
-
-        <?php include 'include/footer.inc.php'; ?>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 </html>
 
