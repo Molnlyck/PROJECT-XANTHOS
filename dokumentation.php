09d6365e64591cce10351711017762f37313436f
Patrick Bergmann <Patrick.bergmann96@gmail.com>
2017-08-31 12:35:39 +0200

    dokumentation

--- dokumentation.php
+++ dokumentation.php
@@ -14,6 +14,7 @@ include_once 'include/config.php';
 
     <!-- CSS -->
     <link rel="stylesheet" href="assets/css/dokumentation.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet" href="https://skole.vibedrive.dk/assets/css/style.css">
     <link rel="stylesheet" href="https://skole.vibedrive.dk/assets/css/general.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -24,7 +25,7 @@ include_once 'include/config.php';
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- FAVICON -->
-    <link rel="shortcut icon" href="http://datamatiker.vibedrive.dk/assets/img/favicon.ico">
+    <link rel="shortcut icon" href="http://datamatiker.vibedrive.dk/assets/img/favicon.ico">s
 
 </head>
 
@@ -32,44 +33,66 @@ include_once 'include/config.php';
 
 <?php include 'include/header.inc.php'; ?>
 
+<!-- Page Content -->
+<div class="container">
 
-<div id="introContainer" class="container-fluid">
     <!-- Introduction Row -->
-    <h1 id="introText" class="my-4">About Us
-        <small>HEJ! her kan du se lidt om os</small>
-    </h1>
-
-    <p id="introTextparagraph">Mange kaldte det umuligt, men i dag har to helte forenet deres kræfter i et fælles forsøg på at skabe en bedre vberden, og et bedre web.</p>
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header my-4">Om Os
+                <small>Hyggeligt at du ville kigge forbi!</small>
+            </h1>
+            <p style="color:#999">Begge projekt medlemmer går på Erhvervsakademiet Lillebælt, beliggende i Vejle.
+                <br>
+                Websiden er et side projekt lavet for både at udbygge personlige egenskaber, samt et sted hvor brugere kan dele deres løste opgaver, eller finde svar på uløste.</p>
+        </div>
+    </div>
 
     <!-- Team Members Row -->
     <div class="row">
         <div class="col-lg-12">
-            <h2 id="teamText" class="my-4">Our Team</h2>
-
-
-            <!-- Patrick Bergmann -->
+            <h2 class="page-header my-4">Vores udviklere</h2>
         </div>
-        <div class="col-lg-6 col-sm-6 text-center mb-6">
-            <img id="picture1" class="rounded-circle img-fluid d-block mx-auto" src="https://cdn.discordapp.com/attachments/350686737007116299/352526002255298570/migpaamc.jpg" alt="">
-            <h3>Patrick Bergmann
-                <small id="task">Officiel team abe</small>
+        <div class="col-lg-6 col-sm-3 text-center">
+            <img class="img-circle img-responsive img-center" src="https://cdn.discordapp.com/attachments/350686737007116299/352526002255298570/migpaamc.jpg" alt="">
+            <h3 class="my-5">Patrick Bergmann
+                <small class="my-small">Officel team abe</small>
             </h3>
-            <p><a href="https://www.facebook.com/MolnlyckCoding/?fref=ts">Facebook</a> || <a href="https://twitter.com/MolnlyckCoding">Twitter</a> || <a href="https://github.com/Molnlyck">GitHub</a> </p>
+            <div class="social">
+                <div class="container-fluid text-center">
+                    <ul class="list-inline">
+                        <li class="list-inline-item social-facebook"><a href="https://www.facebook.com/MolnlyckCoding/?fref=ts"><i class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item social-twitter"><a href="https://twitter.com/MolnlyckCoding"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item social-github"><a href="https://github.com/Molnlyck"><i class="fa fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <p class="my-p">Patrick er en kendt Destiny noob, du kan jævligt finde ham foran sin PS4, savlende over de nyeste raids. <br> Møder du ham en dag på serverne, så er du garanteret en nem sejr.</p>
         </div>
-
-        <!-- Nicklas Vilster -->
-        <div class="col-lg-6 col-sm-6 text-center mb-6">
-            <img id="picture2" class="rounded-circle img-fluid d-block mx-auto" src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/12278730_10153247624051381_4583848965544923573_n.jpg?oh=6a2487f5c1e6fc97f4e67323eb0a102d&oe=5A1BC914" alt="">
-            <h3>Nicklas Vilster
-                <small id="task">Duden der holder styr på team aben</small>
+        <div class="col-lg-6 col-sm-3 text-center">
+            <img class="img-circle img-responsive img-center" src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/12278730_10153247624051381_4583848965544923573_n.jpg?oh=6a2487f5c1e6fc97f4e67323eb0a102d&oe=5A1BC914" alt="">
+            <h3 class="my-5">Nicklas Vilster
+                <small class="my-small">Duden der holder styr på team aben</small>
             </h3>
-            <p><a href="https://www.linkedin.com/in/nicklas-vilster-388704a4/">LinkedIn</a> || <a href="https://twitter.com/Chr0lic">Twitter</a> || <a href="https://github.com/Chrolic">GitHub</a> </p>
+            <div class="social">
+                <div class="container-fluid text-center">
+                    <ul class="list-inline">
+                        <li class="list-inline-item social-linkedin"><a href="https://www.linkedin.com/in/nicklas-vilster-388704a4/"><i class="fa fa-linkedin"></i></a></li>
+                        <li class="list-inline-item social-twitter"><a href="https://twitter.com/Chr0lic"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item social-github"><a href="https://github.com/Chrolic"><i class="fa fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <p class="my-p">Nicklas er awesome!</p>
         </div>
-
     </div>
-</div>
-<!-- /.container -->
+    <!-- END ROW -->
 
+</div>
+
+
+<div class="bgimg"></div>
+<div class="overlaybg"></div>
 
 
 <?php include 'include/footer.inc.php'; ?>
