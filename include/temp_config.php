d2bfe1cb3a51bae4aeecdc4229e08a38f3f4a2f3
Patrick Bergmann <Patrick.bergmann96@gmail.com>
2017-09-05 10:37:31 +0200

    Update temp_config.php

--- include/temp_config.php
+++ include/temp_config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'mysql30.unoeuro.com');
-define('DB_NAME', 'vibedrive_dk_db');
-define('DB_USER', 'vibedrive_dk');
-define('DB_PASSWORD', 'Bergmann123456');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
 
 $temp_conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $temp_conn->set_charset('utf8');
